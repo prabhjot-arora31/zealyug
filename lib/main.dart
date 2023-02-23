8319a55a885461d39f9ba86bb1994d319f661fa2
BigAchiever <danishali9575@gmail.com>
2023-02-23 14:29:49 +0530

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:zealyug/Expertise/expertise.dart';
 import 'package:zealyug/onboardings.dart/onboarding_sliders.dart';
 import 'package:zealyug/onboardings.dart/main_screen.dart';
 import 'package:zealyug/onboardings.dart/onboarding_screens.dart';
@@ -25,7 +24,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Expertise(),
+      home: OnboardingSlider(),
     );
   }
 }
